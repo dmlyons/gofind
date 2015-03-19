20cd20d6fcacb553bfa39d5cee0ca8266ae70afa
David Lyons <david.lyons@returnpath.com>
2015-03-19 10:32:28 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"sync"
+)
+
+type search struct {
+	what, where string
+}
+
+var wg sync.WaitGroup
+
+func main() {
+	var where, what string
+
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	flag.Parse()
+	where = flag.Arg(0)
+	what = flag.Arg(1)
+	fmt.Printf("Searching %s for %s\n", where, what)
+
+	jobs := make(chan search, 100000)
+	results := make(chan string, 10000)
+
+	wnum := 1
+	for i := 1; i <= 100; i++ {
+		go findInDirWorker(wnum, jobs, results)
+		wnum++
+	}
+	wg.Add(1)
+	jobs <- search{what, where}
+	go showResults(results)
+	wg.Wait()
+	close(jobs)
+	close(results)
+}
+
+func showResults(c chan string) {
+	n := 0
+	for found := range c {
+		n++
+		fmt.Printf("%d. %s\n", n, found)
+	}
+}
+
+func findInDirWorker(w int, jobs chan search, result chan<- string) {
+	for j := range jobs {
+		d, err := ioutil.ReadDir(j.where)
+		if err != nil {
+			//fmt.Printf("DirErr: %v\n", err)
+			wg.Done()
+			continue
+		}
+		for _, f := range d {
+			path, err := filepath.Abs(j.where)
+			if err != nil {
+				continue
+			}
+			full := fmt.Sprintf("%s%v%s", path, os.PathSeparator, f.Name())
+			fmt.Println(os.PathSeparator)
+			fmt.Println(full)
+			if strings.Contains(f.Name(), j.what) {
+				res := fmt.Sprintf("%s (%d)", full, w)
+				result <- res
+			}
+			if f.IsDir() {
+				wg.Add(1)
+				jobs <- search{j.what, full}
+			}
+		}
+		wg.Done()
+	}
+}
